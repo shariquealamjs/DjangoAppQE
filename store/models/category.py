6fd4464d131b1699ade15fd8e94dfe1fbaf4feef
Mohd-Sharique-Alam <sharique.alam@tothenew.com>
2022-07-04 13:19:53 +0530

    Second Commit, created navbar, home page done

--- store/models/category.py
+++ store/models/category.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='uploads/categoryimages/', default='/home/ttn/Desktop/onions.jpg')
 
     @staticmethod
     def get_all_categories():
